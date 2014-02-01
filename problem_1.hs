e23c2913f66ccb608cffa8e54ef4260e5f8a9901
Josiah Berkebile <praenato14@gmail.com>
2014-02-01 11:25:33 -0600

    4 solutions for problem 1 using the Maybe monad

--- problem_1.hs
+++ problem_1.hs
@@ -6,9 +6,26 @@
 --  'z'
 
 import Data.Maybe
+import qualified Data.List
 
 myLast :: (Eq a) => [a] -> Maybe a
 myLast xs
   | xs == []  = Nothing
   | otherwise = Just $ head . reverse $ xs
+
+myLast1 :: (Eq a) => [a] -> Maybe a
+myLast1 xs
+  | xs == []  = Nothing
+  | otherwise = Just $ last xs
+
+myLast2 :: (Eq a) => [a] -> Maybe a
+myLast2 xs
+  | xs == []  = Nothing
+  | otherwise = Just $ xs!!lastIdx
+  where lastIdx = length xs - 1
+
+myLast3 :: (Eq a) => [a] -> Maybe a
+myLast3 xs
+  | xs == []  = Nothing
+  | otherwise = Just $ foldl1 (curry snd) xs
 
